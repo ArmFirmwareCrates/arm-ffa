86678b0b45ea556e48998817fb3502fd5dab6d26
Imre Kis <imre.kis@arm.com>
2025-04-11 17:09:09 +0200

    Add fuzzers for serializer/deserializer functions
    Install fuzzer by `cargo install cargo-fuzz` and then run fuzzers using
    `cargo +nightly fuzz run [fuzzer name]`. The available fuzzers are
    boot_info_v1_1, boot_info_v1_2, interface_v1_1, interface_v1_2,
    mem_transaction_desc, partition_info.
    
    TODO: add corpus for each fuzzer
    
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Change-Id: Ic35987b57e6e8dc2b633fa3778a160a138208fe1

---
+++ fuzz/fuzz_targets/boot_info_v1_2.rs
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: Copyright 2025 Arm Limited and/or its affiliates <open-source-office@arm.com>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! Unpacks boot info descriptors from the test data then packs them as defined in FF-A v1.2.
+
+#![no_main]
+
+use arm_ffa::{boot_info::{BootInfo, BootInfoIterator}, Version};
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    let version = Version(1, 2);
+
+    if let Ok(iterator) = BootInfoIterator::new(version, data) {
+        let mut descriptors = Vec::new();
+        for descriptor in iterator {
+            if let Ok(descriptor) = descriptor {
+                descriptors.push(descriptor);
+            } else {
+                return;
+            }
+        }
+
+        let mut buf_out = [0; 4096];
+        BootInfo::pack(version, &descriptors, &mut buf_out, None);
+    }
+});
